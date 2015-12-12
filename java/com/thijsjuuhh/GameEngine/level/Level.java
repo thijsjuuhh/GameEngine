d38adbe76fc2c0863c26e52d6d78a81697152acc
thijsjuuhh <thijsalberts@home.nl>
2015-12-12 18:45:45 +0100

    Reading levels using images

--- java/com/thijsjuuhh/GameEngine/level/Level.java
+++ java/com/thijsjuuhh/GameEngine/level/Level.java
@@ -1,34 +1,57 @@
 package com.thijsjuuhh.GameEngine.level;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Random;
+
+import javax.imageio.ImageIO;
 
 import com.thijsjuuhh.GameEngine.Update;
 import com.thijsjuuhh.GameEngine.graphics.Render2D;
-import com.thijsjuuhh.GameEngine.graphics.SpriteSheet;
+import com.thijsjuuhh.GameEngine.tile.Tile;
 
 public abstract class Level implements Update {
 
 	protected int width, height;
-	protected SpriteSheet s;
+	protected String path;
 	protected static Random r = new Random();
 
-	protected int[] pixels;
+	protected int[] tiles;
 
-	public Level(SpriteSheet s) {
+	public Level(String path) {
 		u.add(this);
-		width = s.getWidth();
-		height = s.getHeight();
-		pixels = new int[width * height];
-		this.s = s;
-		getPixels();
+		this.path = path;
+		getPixels(path);
 	}
 
-	private void getPixels() {
-		for (int y = 0; y < s.getHeight(); y++)
-			for (int x = 0; x < s.getWidth(); x++)
-				pixels[x + y * s.getWidth()] = s.pixels[x + y * s.getWidth()];
+	protected void getPixels(String path) {
+		try {
+			BufferedImage img = ImageIO.read(Level.class.getResource(path));
+			width = img.getWidth();
+			height = img.getHeight();
+			tiles = new int[width * height];
+			for (int y = 0; y < height; y++)
+				for (int x = 0; x < width; x++)
+					tiles[x + y * width] = img.getRGB(x, y);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("COULD NOT FIND LEVEL FILE!");
+		}
 	}
 
 	public abstract void render(int xOffset, int yOffset, Render2D r);
+
+	public abstract Tile getTile(int x, int y);
+
+	public boolean isColliding(int x, int y, int size, int xOffs, int yOffs) {
+		boolean solid = false;
+		for (int c = 0; c < 4; c++) {
+			int xt = (x - c % 2 * size + xOffs) >> 4;
+			int yt = (y - c / 2 * size + yOffs) >> 4;
+			if (getTile(xt, yt).isSolid())
+				solid = true;
+		}
+		return solid;
+	}
 
 }
